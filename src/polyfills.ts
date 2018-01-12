5b1d68eeedaadc240d6a16b986352e251e401756
Brook Hutchinson <brookhutchinson@outlook.com>
2018-01-12 13:44:31 -0600

    Cleanup project files, Change "version" property value to "1.0.0" inside package.json, Change "version" property value to "1.0.0" inside package-lock.json, Change property values inside Angular CLI Config File, Change "prefix" property value from "app" to "" inside .angular-cli.json, Create "css" folder under folder "src/app", Move styles.css from folder "src" to folder "src/app/css", Change "styles" property value file path to "app/css/styles.css" inside .angular-cli.json, Format files, Trim files

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,8 +4,7 @@
  *
  * This file is divided into 2 sections:
  *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
@@ -18,7 +17,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/** IE9, IE10 and IE11 requires all of the following polyfills **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -35,29 +34,29 @@
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+// import 'classlist.js';  // run `npm install --save classlist.js`
 
-/** IE10 and IE11 requires the following for the Reflect API. */
+/** IE10 and IE11 requires the following for the Reflect API */
 // import 'core-js/es6/reflect';
 
 
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+/** evergreen browsers require these. **/
+// used for reflect-metadata in JIT. if you use AOT (and only Angular decorators) you can remove.
 import 'core-js/es7/reflect';
 
 
 /**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
+ * required to support web animations `@angular/platform-browser/animations`
+ * needed for: all but Chrome, Firefox and Opera http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'web-animations-js';  // run `npm install --save web-animations-js`
 
 
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * Zone JS is required by default for Angular itself
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone';  // Included with Angular CLI
 
 
 

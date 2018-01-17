3a9c012f198b0d49f1b6e56a4bffab39576bf407
Brook Hutchinson <brookhutchinson@outlook.com>
2018-01-17 08:29:07 -0600

    Setup Firebase Project, Create new Firebase Project on firebase.google.com named "angular-firebase", Define a new property named "firebase" inside environment.ts that will be set to an object that will contain all of the Firebase Configuration properties key/value pairs, Paste the firebase configuration property key/value pairs inside the "firebase" property object inside environment.ts, Define a new property named "firebase" inside environment.prod.ts that will be set to an object that will contain all of the Firebase Configuration properties key/value pairs, Paste the firebase configuration property key/value pairs inside the "firebase" property object inside environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyABCWXo4z7fO16rc-jqmnYcxZVfTXfBAEA",
+    authDomain: "angular-firebase-f9b91.firebaseapp.com",
+    databaseURL: "https://angular-firebase-f9b91.firebaseio.com",
+    projectId: "angular-firebase-f9b91",
+    storageBucket: "angular-firebase-f9b91.appspot.com",
+    messagingSenderId: "630517786592"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,13 @@
 // the list of which env maps to which file can be found in `.angular-cli.json`
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyABCWXo4z7fO16rc-jqmnYcxZVfTXfBAEA",
+    authDomain: "angular-firebase-f9b91.firebaseapp.com",
+    databaseURL: "https://angular-firebase-f9b91.firebaseio.com",
+    projectId: "angular-firebase-f9b91",
+    storageBucket: "angular-firebase-f9b91.appspot.com",
+    messagingSenderId: "630517786592"
+  }
 };
